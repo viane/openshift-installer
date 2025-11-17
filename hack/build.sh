0bbdf209fccf4e55794211fd4c0f629cc3128ad5
xiaoyu zhou <xiaoyuz2011@gmail.com>
2025-11-16 23:13:18 -0500

    Update build.sh

--- hack/build.sh
+++ hack/build.sh
@@ -21,7 +21,7 @@ export CGO_ENABLED=0
 MODE="${MODE:-release}"
 
 # build cluster-api binaries
-make -C cluster-api all
+make -ddp -n -f -C cluster-api all
 copy_cluster_api_to_mirror
 
 GIT_COMMIT="${SOURCE_GIT_COMMIT:-$(git rev-parse --verify 'HEAD^{commit}')}"
