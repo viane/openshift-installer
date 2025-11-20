27a63beb6c534c16229a58d918e8c80560e63fc5
Allen Zhou <Xiao-Yu.Zhou@ibm.com>
2025-11-20 12:21:54 -0500

    remove providers for fast build

--- cluster-api/providers/azureaso/tools.go
+++
@@ -1,5 +0,0 @@
-package main
-
-import (
-	_ "github.com/Azure/azure-service-operator/v2/cmd/controller"
-)
--- cluster-api/providers/nutanix/tools.go
+++
@@ -1,5 +0,0 @@
-package main
-
-import (
-	_ "github.com/nutanix-cloud-native/cluster-api-provider-nutanix"
-)
